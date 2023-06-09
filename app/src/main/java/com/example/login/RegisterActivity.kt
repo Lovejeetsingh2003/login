4d3495bd2cb87af254c19c5f6d295b9d7102d052
Lovejeetsingh2003 <135810389+Lovejeetsingh2003@users.noreply.github.com>
2023-06-09 18:09:52 +0530

    update

--- app/src/main/java/com/example/login/RegisterActivity.kt
+++ app/src/main/java/com/example/login/RegisterActivity.kt
@@ -1,30 +1,73 @@
 package com.example.login
 
-import  android
+
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var etEmail1 : EditText
     lateinit var etPassword1 : EditText
-    lateinit var erNumber: Number
-    lateinit var llSelector : LinearLayout
+    lateinit var etNumber: EditText
+    lateinit var llSelect : LinearLayout
     lateinit var rgReg :RadioGroup
     lateinit var rbFirst : RadioButton
     lateinit var rbSecond : RadioButton
     lateinit var rbThird: RadioButton
-    lateinit var llRegister1 :LinearLayout
-    lateinit var btnRegister :Button
+    lateinit var etOther : EditText
+    lateinit var llRegister1 : LinearLayout
+    lateinit var btnRegister : Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        etEmail1 = findViewById(R.id.etEmail1)
+        etPassword1 = findViewById(R.id.etPassword1)
+        etNumber = findViewById(R.id.etNumber)
+        llSelect = findViewById(R.id.llSelect)
+        rgReg = llSelect.findViewById(R.id.rgReg)
+        rbFirst = llSelect.findViewById(R.id.rbFirst)
+        rbSecond = llSelect.findViewById(R.id.rbSecond)
+        rbThird = llSelect.findViewById(R.id.rbThird)
+        etOther = llSelect.findViewById(R.id.etOther)
+        llRegister1 = findViewById(R.id.llRegister1)
+        btnRegister = llRegister1.findViewById(R.id.btnRegister)
+
+        btnRegister.setOnClickListener {
+            if (etEmail1.text.toString().isEmpty()) {
+                etEmail1.error = "Enter Your Email"
+            } else if (etPassword1.text.toString().isEmpty()) {
+                etPassword1.error = "Enter your Password"
+            }
+            else if (etNumber.text.toString().isEmpty()) {
+                etNumber.error = "Enter your Mob. No."
+            }else {
+                Toast.makeText(this, "Register Successfully", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+      rgReg.setOnCheckedChangeListener { group, checkedId ->
+
+          if(checkedId == R.id.rbFirst)
+          {
+            etOther.visibility = View.GONE
+          }
+          else if(checkedId == R.id.rbSecond)
+          {
+              etOther.visibility = View.GONE
+          }
+          else {
+              etOther.visibility = View.VISIBLE
+          }
+      }
 
     }
 }
